a936790a22c9468246b8eee81eebe361e0fc7e9d
Michael Olsen <jmolsen91@gmail.com>
2015-09-07 10:40:18 +0200

    Added recyclerview as a suggestion for mood overview, still missing animations

--- app/src/main/java/com/example/michael/voxpop/MainActivity.java
+++ app/src/main/java/com/example/michael/voxpop/MainActivity.java
@@ -1,17 +1,61 @@
 package com.example.michael.voxpop;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import jp.wasabeef.recyclerview.animators.SlideInLeftAnimator;
 
 public class MainActivity extends Activity {
+
+    public static ArrayList<String> moods;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setItemAnimator(new SlideInLeftAnimator());
+        moods = new ArrayList<String>();
+        String[] moodTable = {"yes!", "no!", "this", "is", "a", "cool", "app"};
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(moods, getApplicationContext(), this);
+        mRecyclerView.setAdapter(mAdapter);
+        for(int i=0; i<moodTable.length; i++){
+            moods.add(moodTable[i]);
+            mAdapter.notifyItemInserted(moods.size()-1);
+        }
     }
 
     @Override
@@ -35,4 +79,90 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void clickSubmit(View v) {
+        moods.add("hello");
+        mAdapter.notifyDataSetChanged();
+    }
+
+    public void selectMood(int position){
+        moods.remove(position);
+        mAdapter.notifyDataSetChanged();
+    }
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        private ArrayList<String> mDataset;
+        MainActivity activity;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public TextView mTextView;
+            CardView container;
+
+            public ViewHolder(View v) {
+                super(v);
+                mTextView = (TextView) v.findViewById(R.id.info_text);
+                container = (CardView) v.findViewById(R.id.card_view);
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public MyAdapter(ArrayList<String> myDataset, Context context, MainActivity activity) {
+            mDataset = myDataset;
+            this.activity = activity;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            // create a new view
+
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.row_card, parent, false);
+            // set the view's size, margins, paddings and layout parameters
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            holder.mTextView.setText(mDataset.get(position));
+            CustomClickListener ccl = new CustomClickListener(activity);
+            ccl.setPos(position);
+            holder.container.setOnClickListener(ccl);
+
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.size();
+        }
+
+
+        public class CustomClickListener implements View.OnClickListener {
+            public int position;
+            MainActivity activity;
+
+            public CustomClickListener(MainActivity activity){
+                this.activity = activity;
+
+            }
+            public void setPos(int pos){
+                position = pos;
+            }
+            @Override
+            public void onClick(View v) {
+                activity.selectMood(position);
+            }
+        }
+    }
 }
+
+
