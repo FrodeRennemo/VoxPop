a8177e9577523af4051d02cec5b3bf87a84b8971
Jan Michael Olsen <jmolsen91@gmail.com>
2015-10-01 15:15:42 +0200

    FAM

--- app/src/main/java/com/example/michael/voxpop/FavoritesActivity.java
+++ app/src/main/java/com/example/michael/voxpop/FavoritesActivity.java
@@ -12,7 +12,7 @@ public class FavoritesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("Search");
+        getSupportActionBar().setTitle("Favorites");
     }
 
     @Override
