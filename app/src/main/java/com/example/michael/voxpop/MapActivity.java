0696143d5b20507cbf1ade7cfff59aa77cedde89
Jan Michael Olsen <jmolsen91@gmail.com>
2015-10-28 14:24:48 +0100

    sliding tab view

--- app/src/main/java/com/example/michael/voxpop/MapActivity.java
+++ app/src/main/java/com/example/michael/voxpop/MapActivity.java
@@ -68,7 +68,7 @@ public class MapActivity extends AppCompatActivity implements AsyncListener {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_favorites, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
---
+++ app/src/main/java/com/example/michael/voxpop/TabFeed.java
@@ -0,0 +1,20 @@
+package com.example.michael.voxpop;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hp1 on 21-01-2015.
+ */
+public class TabFeed extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.tab_feed,container,false);
+        return v;
+    }
+}
